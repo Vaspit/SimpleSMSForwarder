f0a239ee689afbe87870286c6a73af6803a2999b
Petr Golovin <vaspit92@gmail.com>
2026-01-07 20:27:55 +0300

    - add sms receiver to manifest; - init DB.

---
+++ app/src/main/kotlin/com/vaspit/simplesmsforwarder/core/data/dao/SmsDao.kt
@@ -0,0 +1,20 @@
+package com.vaspit.simplesmsforwarder.core.data.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.vaspit.simplesmsforwarder.core.data.model.SmsEntity
+
+@Dao
+interface SmsDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertIgnore(entity: SmsEntity): Long
+
+    @Query("SELECT * FROM sms WHERE isSent = 0 ORDER BY timestamp ASC LIMIT :limit")
+    suspend fun getUnsent(limit: Int = 10): List<SmsEntity>
+
+    @Query("UPDATE sms SET isSent = 1 WHERE id = :id")
+    suspend fun markSent(id: Long)
+}
---
+++ app/src/main/kotlin/com/vaspit/simplesmsforwarder/core/data/model/SmsEntity.kt
@@ -0,0 +1,20 @@
+package com.vaspit.simplesmsforwarder.core.data.model
+
+import androidx.room.Entity
+import androidx.room.Index
+import androidx.room.PrimaryKey
+
+@Entity(
+    tableName = "sms",
+    indices = [Index(value = ["uniqueKey"], unique = true)]
+)
+data class SmsEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+    val uniqueKey: String,
+    val sender: String?,
+    val content: String,
+    val timestamp: Long,
+    val isSent: Boolean = false,
+    val lastError: String? = null,
+)
